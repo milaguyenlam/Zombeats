cb4d3e3b3e20b928148118480728b996a4978646
milaguyenlam <44195479+milaguyenlam@users.noreply.github.com>
2019-07-07 19:51:53 +0200

    uploaded test assets,

---
+++ Zombeats/Assets/Scripts/enemy1_controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy1_controller : MonoBehaviour
+{
+    public BoxCollider2D cb;
+    public float walkSpeed;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 vector = new Vector2(0.0f, -1.0f);
+        rb.velocity = vector * walkSpeed;
+    }
+}
